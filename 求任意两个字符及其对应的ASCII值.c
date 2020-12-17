814548b0baf7bb3f4a95ab68ea7cd02baf5d3447
Sun-capital <39646463@qq.com>
2020-12-17 22:43:28 +0800

    字符转换

---
+++ 求任意两个字符及其对应的ASCII值.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+
+    char ch1,ch2;
+	ch1='a';
+	ch2='b';
+	printf("ch1=%c,ch2=%c\n",ch1,ch2);
+	printf("ch1的ASCII的值为:%d,ch2的值ASCII值为:%d\n",ch1,ch2);
+	return 0;
+	
+}
