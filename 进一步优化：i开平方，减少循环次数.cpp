fbed8304fa5fcfd41f421255c213abc6c044b44e
Sun-capital <39646463@qq.com>
2021-01-03 23:31:32 +0800

    2021-1-3

---
+++ 进一步优化：i开平方，减少循环次数.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int i=0;
+	int count=0;
+	for(i=101;i<=200;i+=2)
+	{
+	//ÅÐ¶ÏiÊÇ·ñÎªËØÊý
+	int j=0
+   for(j=2;j<sqrt(i);j++)
+	{
+		if(i%j==0)
+		break;
+	 } 
+	 if(j>sqrt(i)
+	 {
+	 count++
+	  prinft("%d",i);	
+	 }
+}
+prinft("\ncount=%d\n",count);
+return 0;
+}
