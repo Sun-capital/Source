869a5da7d292b4efa2892ab33844d8e9ed616488
Sun-capital <39646463@qq.com>
2021-06-12 18:06:57 +0800

    打印成绩

---
+++ 2021_6.12(2.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	char arr[100];
+	printf("           2021年上半年全国大学英语六级考试（CTE6）成绩查询\n       ");
+	printf("        姓 名：巩静雯\n ");
+	printf("              学 校：青岛理工大学琴岛学院\n");
+	printf("\n");
+	printf("                           笔试成绩\n");
+	printf("\n");
+	
+	printf("                 总 分：666\n");
+	printf("                 听 力：235\n");
+	printf("                 阅 读：220\n");
+	printf("                 写作和翻译：211");
+
+
+		
+		
+	return 0;
+}
