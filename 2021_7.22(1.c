8e53d85d86438e264850be4511091750a9d378d5
Sun-capital <39646463@qq.com>
2021-07-22 23:15:29 +0800

    函数封装二分查找

---
+++ 2021_7.22(1.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+int binary_search(int arr[], int k, int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else
+			return mid;
+	}
+	return -1;
+}
+int main()
+{
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int ret = binary_search(arr, k, 7);
+	if (ret == -1)
+	{
+		printf("没有找到");
+
+	}
+	else
+	{
+		printf("找到了，是：%d", ret);
+	}
+	return 0;
+}
