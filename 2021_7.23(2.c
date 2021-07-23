04c259334fa19ed79a987b0472cbf8ee3c50c678
Sun-capital <39646463@qq.com>
2021-07-23 16:39:10 +0800

    函数封装判断是否是闰年

---
+++ 2021_7.23(2.c
@@ -0,0 +1,22 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int leap(int year)
+//{
+//	return (year % 100 == 0 && year % 4 == 0 || year % 400 == 0);
+//}
+//int main()
+//{
+//	int year;
+//	int ret;
+//	scanf("%d", &year);
+//	ret = leap(year);
+//	if (ret == 1)
+//	{
+//		printf("%d是闰年", year);
+//	}
+//	if (ret == 0)
+//	{
+//		printf("%d不是闰年", year);
+//	}
+//	return 0;
+//}
