232bab0476e256df04482e817f5fd86f6112515c
Sun-capital <39646463@qq.com>
2021-07-23 16:37:29 +0800

    函数判断素数是否存在

---
+++ 2021_7.23(1.c
@@ -0,0 +1,26 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int prime(int x)
+//{
+//	int j;
+//	for (j = 2; j < x; j++)
+//	{
+//		if (x % j == 0)
+//			return 0;
+//		else
+//			return 1;
+//	}
+//}
+//int main()
+//{
+//	int i = 0;
+//	for (i = 100; i < 200; i++)
+//	{
+//		if (prime(i) == 1)
+//		{
+//			printf("%d\n", i);
+//		}
+//		
+//	}
+//	return 0;
+//}
