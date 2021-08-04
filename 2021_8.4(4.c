0ea28e5ad1388a82dcae799798876eeb52109264
Sun-capital <39646463@qq.com>
2021-08-04 22:41:15 +0800

    高效率实现strlen

---
+++ 2021_8.4(4.c
@@ -0,0 +1,23 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#include<assert.h>
+//int My_strlen(const char* parr)
+//{
+//	assert(parr != NULL);
+//	int count = 0;
+//	while (*parr)
+//	{
+//		count++;
+//		parr++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	char arr[100] = { 0 };
+//	int ret = 0;
+//	gets(arr);
+//	ret = My_strlen(arr);
+//	printf("%d", ret);
+//	return 0;
+//}
