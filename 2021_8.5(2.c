3e5d5ea78b6b2cde8307cc749be68528f544864f
Sun-capital <39646463@qq.com>
2021-08-05 23:15:05 +0800

    按位异或交换数值

---
+++ 2021_8.5(2.c
@@ -0,0 +1,13 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int main()
+//{
+//	int x = 19;
+//	int y = 30;
+//	printf("交换前：%d %d \n",x,y );
+//	x = x ^ y;
+//	y = y ^ x;
+//	x = x ^ y;
+//	printf("交换后：%d %d", x, y);
+//	return 0;
+//}
