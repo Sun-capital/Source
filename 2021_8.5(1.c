8227a23fbff656a86f2ee499ae145b6ef0a765b4
Sun-capital <39646463@qq.com>
2021-08-05 22:51:36 +0800

    计算二级制中1的个数

---
+++ 2021_8.5(1.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int Manybit(int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		n = n & (n - 1);
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int n = 0;
+//	int sum = 0;
+//	scanf("%d", &n);
+//	sum=Manybit(n);
+//	printf("%d", sum);
+//	return 0;
+//}
