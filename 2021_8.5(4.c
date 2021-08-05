3458695cf1f0c34acb11e4d3a1696eb3ca6ff25e
Sun-capital <39646463@qq.com>
2021-08-05 23:18:32 +0800

    计算两数二级制为有都是不同的位数

---
+++ 2021_8.5(4.c
@@ -0,0 +1,26 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//int Defeince(int x, int y)
+//{
+//	int i = 0;
+//	int ret = x ^ y;
+//	int count = 0;
+//	while (ret)
+//	{
+//		ret = ret & (ret - 1);
+//		count++;
+//	}
+//	
+//	
+//	return count;
+//}
+//int main()
+//{
+//	int num1, num2;
+//	int ret = 0;
+//	scanf("%d %d", &num1, &num2);
+//
+//	ret = Defeince(num1, num2);
+//	printf("%d", ret);
+//	return 0;
+//}
